ee4e9994294b47dfdfcaf9c77b2e2f921a0e7df0
dreytted <victorkiariem@gmail.com>
2025-05-14 07:34:34 +0300

    login otp code

---
+++ Utils/otp_generator.py
@@ -0,0 +1,16 @@
+import random
+from accounts.models import Otp
+
+
+def otp_generate():
+
+    length = 6
+    otp = ''
+
+    while True:
+        otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])
+        if Otp.objects.filter(otp_code=otp).exists():
+            continue
+        else:
+            break
+    return otp
